d121eb03b5ebd9a1a52f5e5a793e1960997969e7
Gabriela Wilkison <gabrielawilkison@Gabrielas-MacBook-Pro.local>
2022-08-01 18:47:49 -0700

    max of two lab

---
+++ maxOfTwo.js
@@ -0,0 +1,20 @@
+let x = 1
+let y = 3
+
+// Write your code below this line.
+
+
+// Write a block of code that will print out the largest value.
+
+// Using the given values as an example, your code should print out 2.
+
+// To verify your code is working properly, try and change the values of x and y.
+
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
+
+if(x > y){
+    console.log(x)
+}else{
+    console.log(y)
+}
