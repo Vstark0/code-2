99782c02d839be8e2d022f405d3506c6874a3b62
Gabriela Wilkison <gabrielawilkison@Gabrielas-MacBook-Pro.local>
2022-08-04 07:38:53 -0700

    palindrome stretch

---
+++ isPalindrome.js
@@ -0,0 +1,14 @@
+// Write your code below
+//return true if word is palindrome and false if not
+
+//for loop
+
+//.reverse array method
+
+const isPalindrome = (str) => {
+    let reverseStr = str.split('').reverse().join('');
+    return str === reverseStr;
+
+}
+console.log(isPalindrome("Racecar"))
+
