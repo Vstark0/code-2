410dc259bdf4c59e2893143b211cf34b6af0c8f4
Gabriela Wilkison <gabrielawilkison@Gabrielas-MacBook-Pro.local>
2022-08-30 20:33:46 -0700

    indexjs for backend 1 demo

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-const express = require("express");
-const cors = require("cors");
+// submit buttons
+const getSubmit = document.getElementById('getSubmit');
+const getParamsSubmit = document.getElementById('getParamsSubmit');
+const getQuerySubmit = document.getElementById('getQuerySubmit');
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+// inputs
+const paramsInput = document.getElementById('params-input');
+const queryInput = document.getElementById('query-input');
 
+// response section
+const responseSection = document.getElementsByClassName('response-area')[0];
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+// handle submits
+getSubmit.addEventListener('click', () => {
+    axios
+        .get('http://localhost:5050/api/inventory')
+        .then(res => addToView(res.data))
+});
 
-app.get("/api/users")
+getParamsSubmit.addEventListener('click', () => {
+    axios
+        .get(`http://localhost:5050/api/inventory/${paramsInput.value}`)
+        .then(res => addToView([res.data]))
+});
 
+getQuerySubmit.addEventListener('click', () => {
+    axios
+        .get(`http://localhost:5050/api/inventory?item=${queryInput.value}`)
+        .then(res => addToView(res.data))
+});
+
+// handle response
+function addToView(dataArr) {
+    responseSection.innerHTML = null;
+
+    if (dataArr.length === 0) {
+        const p = document.createElement('p');
+        const t = document.createTextNode("Response came back with no results!");
+        p.appendChild(t);
+
+        responseSection.appendChild(p)
+    } else {
+        dataArr.forEach(item => {
+            const p = document.createElement('p');
+            const t = document.createTextNode(item)
+            p.appendChild(t);
+    
+            responseSection.appendChild(p)
+        })
+    }
+}
